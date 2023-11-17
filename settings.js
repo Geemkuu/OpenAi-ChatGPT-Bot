5a4c7495800cc6e372590a88d03720701382f07b
Geemkuu <144843941+Geemkuu@users.noreply.github.com>
2023-11-17 13:04:56 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ const chalk = require('chalk')
 */
 global.OFFLINE = false // if you want put bot offline put true
 global.BOT_PRIVATE = true // if you want use bot privet 
-global.AUTO_CHATGPT = false // if you want auto ai bot mod
+global.AUTO_CHATGPT = true // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
 global.ONLY_GROUPS = false // Only Groups Use
@@ -16,12 +16,12 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
+global.OWNER_NAME = '亗『ＧＥＥ』亗' // Owner Name
+global.OWNER_NUMBER = ['254794286079'] //Owner Number
+global.BOT_ADMINS = ['254794286079','254700569709'] //Bot admins numbers Like a Sudo Numbers
 global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
-global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
-global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
+global.IMAGE_QUALITY = 'high' // Available Quality Are => low | mediam | high
+global.CAPTION = '*by 亗『ＧＥＥ』亗n͜͡önesence ×*'
 
 /*
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
@@ -44,8 +44,8 @@ mess = {
 	need_text: '*Please Give Some words*'
 }
 react = {
-    ai_message : '🤖',
-    ai_image : '🤖'
+    ai_message : '🚮',
+    ai_image : '😂'
 }
 
 
